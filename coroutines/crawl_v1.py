da179a64bef52037c1d5958ccc7a34448a3614e9
mianxiang <zxiaohui2000@163.com>
2022-01-19 23:06:38 +0800

    coroutine study

---
+++ coroutines/crawl_v1.py
@@ -0,0 +1,16 @@
+import time
+
+def crawl_page(url):
+    print('crawling {}'.format(url))
+    sleep_time = int(url.split('_')[-1])
+    time.sleep(sleep_time)
+    print('OK {}'.format(url))
+
+def main(urls):
+    for url in urls:
+        crawl_page(url)
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    main(['url_1', 'url_2', 'url_3', 'url_4'])
+    print('Overall execution time: {}'.format(time.perf_counter() - start))
---
+++ coroutines/crawl_v2.py
@@ -0,0 +1,18 @@
+import asyncio
+import time
+
+async def crawl_page(url):
+    print('crawling {}'.format(url))
+    sleep_time = int(url.split('_')[-1])
+    await asyncio.sleep(sleep_time)
+    print('OK {}'.format(url))
+
+async def main(urls):
+    for url in urls:
+        await crawl_page(url)
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    asyncio.run(main(['url_1', 'url_2', 'url_3', 'url_4']))
+    end = time.perf_counter()
+    print('Overall exeuction time:{}'.format(end - start))
